4f57a13707121e736cc87d8692e710d5673bb272
M1seryy <62029606+M1seryy@users.noreply.github.com>
2023-04-27 21:46:44 +0300

    task1

---
+++ js/01-gallery.js
@@ -0,0 +1,39 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+// console.log(galleryItems);
+const ul = document.querySelector(".gallery");
+
+// instance.show()
+ul.addEventListener("click", urlHandler);
+let newArr = galleryItems
+  .map((item) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="#">
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      data-source=${item.original}
+      alt=${item.description}
+    />
+  </a>
+</li>`;
+  })
+  .join("");
+let img;
+ul.insertAdjacentHTML("afterbegin", newArr);
+function createMyLigthBox(dataAtr) {
+  const instance = basicLightbox.create(`
+    <img src=${dataAtr} width="800" height="600">
+    `);
+  img = instance;
+}
+function urlHandler(e) {
+  createMyLigthBox(e.target.getAttribute("data-source"));
+  img.show();
+}
+
+window.addEventListener("keydown", (e) => {
+  if (e.keyCode === 27) {
+    img.close();
+  }
+});
