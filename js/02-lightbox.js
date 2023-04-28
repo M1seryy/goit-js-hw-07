c58614d0cb8286896588204cb442711355b5f403
M1seryy <62029606+M1seryy@users.noreply.github.com>
2023-04-28 18:42:57 +0300

    task2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+const ul = document.querySelector(".gallery");
 
-console.log(galleryItems);
+// instance.show()
+let newArr = galleryItems
+  .map((item) => {
+    return `<li class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      data-source=${item.original}
+      alt=${item.description}
+    />
+  </a>
+</li>`;
+  })
+  .join("");
+ul.insertAdjacentHTML("afterbegin", newArr);
+var lightbox = new SimpleLightbox(".gallery__link", {
+  captionsData: "alt",
+  captionDelay: "250",
+  captionPosition: "bottom",
+});
